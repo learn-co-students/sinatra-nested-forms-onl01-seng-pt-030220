8d159f1c84bd1a1ebbef4f643d5c674e8dddd14f
Jade Lynn Masker <hopefulfire@gmail.com>
2020-05-29 16:54:08 -0400

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+
+	@@all = []
+
+	attr_accessor :name, :type, :booty
+
+	def initialize(attributes)
+		attributes.each do |k, v|
+			self.send("#{k}=", v)
+		end
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
+
+	def self.clear
+		@@all.clear
+	end
+	
 end
